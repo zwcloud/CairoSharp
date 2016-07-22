fab28c13ebb78cb9b823dfdd2c17d14a6c18c2e3
zwcloud <zwcloud@yeah.net>
2016-07-23 07:30:25 +0800

    Adding sample project converted from cairo website.
    https://www.cairographics.org/samples/

---
+++ CairoSamples/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CairoSamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.selectASampleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectASampleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(574, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // selectASampleToolStripMenuItem
+            // 
+            this.selectASampleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arcToolStripMenuItem});
+            this.selectASampleToolStripMenuItem.Name = "selectASampleToolStripMenuItem";
+            this.selectASampleToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.selectASampleToolStripMenuItem.Text = "Select a sample";
+            // 
+            // arcToolStripMenuItem
+            // 
+            this.arcToolStripMenuItem.Name = "arcToolStripMenuItem";
+            this.arcToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.arcToolStripMenuItem.Text = "arc";
+            this.arcToolStripMenuItem.Click += new System.EventHandler(this.arcToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 405);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem selectASampleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arcToolStripMenuItem;
+    }
+}
+
---
+++ CairoSamples/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Forms;
+using Cairo;
+using Color = Cairo.Color;
+using Graphics = System.Drawing.Graphics;
+
+namespace CairoSamples
+{
+    public partial class Form1 : Form
+    {
+        public Graphics Graphics1 { get; private set; }
+        public Context Context1 { get; set; }
+        public Win32Surface Surface1 { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private System.Action<Context> OnPaintAction;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics1 = e.Graphics;
+            Surface1 = new Win32Surface(Graphics1.GetHdc());
+            Context1 = new Context(Surface1);
+
+            if (OnPaintAction!=null)
+            {
+                OnPaintAction(Context1);
+            }
+
+            Graphics1.Dispose();
+            Context1.Dispose();
+            Surface1.Dispose();
+        }
+
+        private void arcToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            OnPaintAction = (cr) =>
+            {
+                double xc = 128.0;
+                double yc = 128.0;
+                double radius = 100.0;
+                double angle1 = 45.0 * (System.Math.PI / 180.0);  /* angles are specified */
+                double angle2 = 180.0 * (System.Math.PI / 180.0);  /* in radians           */
+
+                cr.LineWidth = 10.0;
+                cr.Arc(xc, yc, radius, angle1, angle2);
+                cr.Stroke();
+
+                /* draw helping lines */
+                cr.SetSourceRGBA(1, 0.2, 0.2, 0.6);
+                cr.LineWidth = 6.0;
+
+                cr.Arc(xc, yc, 10.0, 0, 2 * System.Math.PI);
+                cr.Fill();
+
+                cr.Arc(xc, yc, radius, angle1, angle1);
+                cr.LineTo(xc, yc);
+                cr.Arc(xc, yc, radius, angle2, angle2);
+                cr.LineTo(xc, yc);
+                cr.Stroke();
+            };
+
+            Invalidate();
+        }
+
+    }
+}
